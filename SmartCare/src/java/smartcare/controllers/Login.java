50e273859d5af371249e71d0b49488b4671d8d9b
Anh Minh <minhla@dgroup.co>
2020-12-03 14:50:57 +0700

    Added user models

---
+++ SmartCare/src/java/smartcare/controllers/Login.java
@@ -0,0 +1,9 @@
+package smartcare.controllers;
+
+/**
+ *
+ * @author minh-la
+ */
+public class Login {
+    //TODO: implement authentication for patients, doctors and nurses
+}
---
+++ SmartCare/src/java/smartcare/models/Doctor.java
@@ -0,0 +1,43 @@
+package smartcare.models;
+
+public class Doctor {
+    private String doctorID;
+    private String name;
+    private String username;
+    private String password;
+
+    public String getDoctorID() {
+        return doctorID;
+    }
+
+    public void setDoctorID(String doctorID) {
+        this.doctorID = doctorID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+}
