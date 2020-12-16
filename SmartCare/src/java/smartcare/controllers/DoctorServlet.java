21b78f87e18af20c734f9207a67b255d2edaa0f2
Michael Tonkin <michaeltonkin99@gmail.com>
2020-12-16 13:07:22 +0000

    Tidied and merged with Filter

--- SmartCare/src/java/smartcare/controllers/DoctorServlet.java
+++ SmartCare/src/java/smartcare/controllers/DoctorServlet.java
@@ -3,35 +3,23 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-<<<<<<< HEAD:SmartCare/src/java/smartcare/controllers/DoctorServlet.java
 package smartcare.controllers;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDate;
 import javax.servlet.RequestDispatcher;
-=======
-package smartcare.controllers.landings;
-
-import java.io.IOException;
-import java.io.PrintWriter;
->>>>>>> Filters:SmartCare/src/java/smartcare/controllers/landings/AdminServlet.java
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import smartcare.models.database.Jdbc;
 
-<<<<<<< HEAD:SmartCare/src/java/smartcare/controllers/DoctorServlet.java
 /**
  *
  * @author jitojar
  */
 public class DoctorServlet extends HttpServlet {
-=======
-@WebServlet(name = "AdminServlet", urlPatterns = {"/AdminLanding.do"})
-public class AdminServlet extends HttpServlet {
->>>>>>> Filters:SmartCare/src/java/smartcare/controllers/landings/AdminServlet.java
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -42,7 +30,6 @@ public class AdminServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-<<<<<<< HEAD:SmartCare/src/java/smartcare/controllers/DoctorServlet.java
     
     private HttpServletRequest showAppointments(HttpServletRequest request){
         
@@ -80,23 +67,6 @@ public class AdminServlet extends HttpServlet {
         
         RequestDispatcher view = request.getRequestDispatcher(viewPath);
         view.forward(request,response);
-=======
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet AdminServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet AdminServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
->>>>>>> Filters:SmartCare/src/java/smartcare/controllers/landings/AdminServlet.java
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
