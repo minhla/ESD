e3000e50e3e06d364a0b1261f30a3c338d6136c4
Anh Minh <minhla@dgroup.co>
2020-12-28 23:31:32 +0700

    Added more  sample users in the SQL script

--- SQL/Users.sql
+++ SQL/Users.sql
@@ -15,3 +15,8 @@ INSERT INTO USERS (firstname, lastname, usertype, dob, phone, email, address, pa
 INSERT INTO USERS (firstname, lastname, usertype, dob, phone, email, address, password) VALUES ('Asia', 'Benyadilok', 'P', '09/01/2015', '911', 'bas@email.com', 'Thailand?', 'bas');
 INSERT INTO USERS (firstname, lastname, usertype, dob, phone, email, address, password) VALUES ('Mihn', 'Leeee', 'N', '01/01/2015', '000', 'mihn@email.com', 'Other Thailand', 'pass');
 INSERT INTO USERS (firstname, lastname, usertype, dob, phone, email, address, password) VALUES ('David', 'Giac', 'N', '02/01/2016', '666', 'daveandgiac@email.com', 'Mediterranean', 'pass123');
+
+INSERT INTO USERS (firstname, lastname, usertype, dob, phone, email, address, password) VALUES ('Admin', 'Albedo', 'A', '09/01/1980', '999', 'admin@gmail.com', 'UWE', 'admin');
+INSERT INTO USERS (firstname, lastname, usertype, dob, phone, email, address, password) VALUES ('Doctor', 'David', 'D', '12/06/1980', '999', 'doctor@gmail.com', 'UWE', 'doctor');
+INSERT INTO USERS (firstname, lastname, usertype, dob, phone, email, address, password) VALUES ('Patient', 'Peter', 'P', '06/03/1995', '999', 'patient@gmail.com', 'UWE', 'patient');
+INSERT INTO USERS (firstname, lastname, usertype, dob, phone, email, address, password) VALUES ('Nurse', 'Nemo', 'N', '04/03/1993', '999', 'nurse@gmail.com', 'UWE', 'nurse');
