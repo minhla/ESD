9354309dccef34bcf0fc959c0dbc47e80d595bf9
Anh Minh <minhla@dgroup.co>
2021-01-05 11:39:47 +0700

    Changed API key to a new, restricted one

--- SmartCare/web/views/javascript/map.js
+++ SmartCare/web/views/javascript/map.js
@@ -30,7 +30,7 @@ function initMap() {
     
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 10,
+      zoom: 11,
       center: uluru,
       gestureHandling: "cooperative",
       zoomControl: false
