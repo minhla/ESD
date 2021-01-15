ff31e26e2c8dc8c4018a8d0f57cf793a22cff0e9
Michael Tonkin <michaeltonkin99@gmail.com>
2021-01-15 12:20:22 +0000

    Updates to sql

--- SQL/Users.sql
+++ SQL/Users.sql
@@ -1,15 +1,13 @@
-CREATE TABLE users (
-    username VARCHAR(35) PRIMARY KEY,
-    firstname VARCHAR(30) NOT NULL,
-    lastname VARCHAR(30) NOT NULL,
-    usertype VARCHAR(1) NOT NULL,
-    dob DATE,
-    regdate DATE,
-    phone VARCHAR(11),
-    email VARCHAR(64),
-    address VARCHAR(70),
-    password VARCHAR(30) NOT NULL
-);
-
-
-INSERT INTO USERS (username, firstname, lastname, usertype, dob, phone, email, address, password, regdate) VALUES ('m-tonkin', 'Michael', 'Tonkin', 'A', '1999-01-09', '999', 'michael@email.com', 'UWE', 'admin_password', '2020-12-20');
+create table "SMARTCARE".USERS
+(
+	USERNAME VARCHAR(35) not null primary key,
+	FIRSTNAME VARCHAR(30) not null,
+	LASTNAME VARCHAR(30) not null,
+	USERTYPE VARCHAR(1) not null,
+	DOB DATE,
+	REGDATE DATE,
+	PHONE VARCHAR(11),
+	EMAIL VARCHAR(64),
+	ADDRESS VARCHAR(70),
+	PASSWORD VARCHAR(60) not null
+)
